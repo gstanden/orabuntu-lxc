43faee775aae6e10b6892518c39cb6ae33830eee
Gilbert Standen <gstanden@users.noreply.github.com>
2015-12-01 14:32:38 -0600

    Update ubuntu-services-0.sh

--- ubuntu-services-0.sh
+++ ubuntu-services-0.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
+echo "==============================="
 echo "The gstanden user is currently needed for this release of the software to work."
 echo "Therefore, the gstanden user is created below"
 echo "Accept defaults for all settings.  Set whatever password you want for the account."
 echo "After this script runs, the Ubuntu host will reboot."
 echo "After reboot, login as the gstanden user and run the orabuntu-lxc scripts."
+echo "==============================="
+echo ''
 sudo adduser gstanden
 sudo adduser gstanden sudo
 sudo wget -P /home/gstanden/Downloads/ https://github.com/gstanden/orabuntu-lxc/archive/master.zip
