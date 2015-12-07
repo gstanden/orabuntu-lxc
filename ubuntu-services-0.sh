2bf6f51f9da3866144044063b9a5239cd72f81ba
gstanden <gilstanden@hotmail.com>
2015-12-07 11:47:02 -0600

    v13.4 (Updates to ubuntu-services-0.sh)

--- ubuntu-services-0.sh
+++ ubuntu-services-0.sh
@@ -7,18 +7,16 @@ echo "After this script runs, the Ubuntu host will reboot."
 echo "After reboot, login as the gstanden user and run the orabuntu-lxc scripts."
 echo "==============================="
 echo ''
-echo "Script execution will continue in 15 seconds..."
+echo "Script execution will continue in 5 seconds..."
 echo ''
-sleep 15
+sleep 5
 sudo adduser gstanden
 sudo adduser gstanden sudo
 sudo wget -P /home/gstanden/Downloads/orabuntu-lxc-master/ https://github.com/gstanden/orabuntu-lxc/archive/master.zip
-sudo unzip /home/gstanden/Downloads/orabuntu-lxc-master/master.zip -d /home/gstanden/Downloads/orabuntu-lxc-master
-cd /home/gstanden/Downloads/orabuntu-lxc-master/orabuntu-lxc-master/
-sudo cp -pr * ../.
+sudo unzip /home/gstanden/Downloads/orabuntu-lxc-master/master.zip -d /home/gstanden/Downloads/
 echo ''
 echo "Rebooting in 15 seconds"
-echo "Login as gstanden after reboot and cd to /home/gstanden/Downloads/orabuntu-lxc-master/ and run ubuntu-services-1.sh to begin install."
+echo "Login as gstanden after reboot and cd to /home/gstanden/Downloads/orabuntu-lxc-master/ and run ubuntu-services.sh to begin install."
 sleep 15
 sudo reboot
 
