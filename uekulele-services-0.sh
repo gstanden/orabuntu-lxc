1bb7ce84554cc6cea10118b093bab6a8c7ea5ef1
gstanden <gstanden>
2017-10-01 12:04:25 -0500

    v5.0-beta Orabuntu-LXC

--- uekulele-services-0.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-adduser ubuntu
-passwd ubuntu
-usermod -aG wheel ubuntu
-cd /home/ubuntu
-mkdir -p  Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
-chown ubuntu:ubuntu Desktop  Documents  Downloads  Music  Pictures  Public  Templates  Videos
-
