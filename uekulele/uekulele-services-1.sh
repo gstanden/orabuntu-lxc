fbc29d7c75f14ddd4c8ec3ed6c4d8e43d9e476e4
Gilbert Standen <gilbert@orabuntu-lxc.com>
2018-03-05 18:53:27 -0600

    v6.0-beta AMIDE
    Labelling version

--- uekulele/uekulele-services-1.sh
+++ uekulele/uekulele-services-1.sh
@@ -14,7 +14,7 @@
 #    GNU General Public License for more details.
 
 #    You should have received a copy of the GNU General Public License
-#    along with Orabuntu-LXC.  If not, see <http://www.gnu.org/licenses/>.
+#    along with Orabuntu-LXC. If not, see <http://www.gnu.org/licenses/>.
 
 #    v2.4 		GLS 20151224
 #    v2.8 		GLS 20151231
