09818b6f2da8585e83b2192c58a8da3270daaa83
gstanden <gilstanden@hotmail.com>
2021-01-08 04:06:33 +0000

    v6.13.20-beta AMIDE

--- orabuntu/archives/docker.sh
+++
@@ -1,8 +0,0 @@
-        sudo apt-get -y update
-        sudo apt-get -y install apt-transport-https ca-certificates curl software-properties-common
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-        sudo apt-key fingerprint 0EBFCD88
-        sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu zesty stable"
-        sudo apt-get -y update
-        sudo apt-get -y install docker-ce
-
