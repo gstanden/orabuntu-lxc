8b27bb65f200232d193b2af8fbd489608c6f00de
gstanden <gilstanden@hotmail.com>
2021-01-09 23:27:22 -0600

    v6.13.20-beta AMIDE

--- anylinux/anylinux-services.GRE.HOST.sh
+++ anylinux/anylinux-services.GRE.HOST.sh
@@ -56,6 +56,7 @@ echo "=============================================="
 echo ''
 
 trap "exit" INT TERM; trap "kill 0" EXIT; sudo -v || exit $?; sleep 1; while true; do sleep 60; sudo -nv; done 2>/dev/null &
+sudo date
 
 echo ''
 echo "=============================================="
