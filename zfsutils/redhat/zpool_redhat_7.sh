96495d5660102fab64ba6cd27e0dc512c2cc85b0
gstanden <gilstanden@hotmail.com>
2021-10-02 11:37:12 -0500

    v7.0.0-alpha ELENA

---
+++ zfsutils/redhat/zpool_redhat_7.sh
@@ -0,0 +1,202 @@
+#!/bin/bash
+
+# Usage:  ./zpool_redhat_7.sh [olxc-001|olxc-002|olxc-003|...]
+
+clear
+
+echo ''
+echo "=============================================="
+echo "Configure ZFS Storage ...                     "
+echo "CentOS7 OpenZFS build/install rpm from source."
+echo "                                              "
+echo "(rpm build takes awhile ... patience)         " 
+echo "=============================================="
+echo ''
+
+sleep 5
+
+clear
+
+echo ''
+echo "=============================================="
+echo "Establish sudo...                             "
+echo "=============================================="
+echo ''
+
+trap "exit" INT TERM; trap "kill 0" EXIT; sudo -v || exit $?; sleep 1; while true; do sleep 60; sudo -nv; done 2>/dev/null &
+sudo date
+
+echo ''
+echo "=============================================="
+echo "Done: Establish sudo.                         "
+echo "=============================================="
+echo ''
+
+sleep 5
+
+clear
+
+echo ''
+echo "=============================================="
+echo "Enable required repos ...                     "
+echo "=============================================="
+echo ''
+
+sudo yum -y install --enablerepo=epel python-packaging dkms
+sudo subscription-manager repos --enable rhel-7-server-optional-rpms --enable rhel-7-server-extras-rpms
+sudo yum install https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm
+
+echo ''
+echo "=============================================="
+echo "Done: Enable required repos.                  "
+echo "=============================================="
+echo ''
+
+sleep 5
+
+clear
+ 
+echo ''
+echo "=============================================="
+echo "Install packages ...                          "
+echo "=============================================="
+echo ''
+
+sudo yum -y install unzip wget openssh-server net-tools bind-utils
+sudo yum -y install epel-release gcc make autoconf automake libtool rpm-build libtirpc-devel libblkid-devel libuuid-devel libudev-devel openssl-devel zlib-devel libaio-devel libattr-devel elfutils-libelf-devel kernel-devel-$(uname -r) python python2-devel python-setuptools python-cffi libffi-devel git ncompress
+
+echo ''
+
+sleep 5
+
+clear
+
+echo ''
+echo "=============================================="
+echo "Clone OpenZFS git repo...                     "
+echo "=============================================="
+echo ''
+
+git clone https://github.com/openzfs/zfs
+cd ./zfs
+git checkout master
+
+echo ''
+echo "=============================================="
+echo "Done: Clone OpenZFS git repo.                 "
+echo "=============================================="
+echo ''
+
+sleep 5
+
+clear
+
+echo ''
+echo "=============================================="
+echo "Run autogen and configure...                  "
+echo "=============================================="
+echo ''
+
+sh autogen.sh
+./configure
+
+echo ''
+echo "=============================================="
+echo "Done: Run autogen and configure.              "
+echo "=============================================="
+echo ''
+
+sleep 5
+
+clear
+
+echo ''
+echo "=============================================="
+echo "make rpm ...                                  "
+echo "=============================================="
+echo ''
+
+make rpm
+
+echo ''
+echo "=============================================="
+echo "Done: make rpm.                               "
+echo "=============================================="
+echo ''
+
+sleep 5
+
+clear
+
+echo ''
+echo "=============================================="
+echo "Install additional packages ...               "
+echo "=============================================="
+echo ''
+
+sudo yum -y install sysstat mdadm ksh fio
+
+echo ''
+echo "=============================================="
+echo "Done: Install additional packages.            "
+echo "=============================================="
+echo ''
+
+sleep 5
+
+clear
+
+echo ''
+echo "=============================================="
+echo "Install OpenZFS packages...                   "
+echo "=============================================="
+echo ''
+
+sudo rpm -ivh *.rpm
+
+echo ''
+echo "=============================================="
+echo "Done: Install OpenZFS packages.               "
+echo "=============================================="
+echo ''
+
+sleep 5
+
+clear
+
+echo ''
+echo "=============================================="
+echo "Finish OpenZFS configuration...               "
+echo "=============================================="
+echo ''
+
+sudo modprobe zfs
+sudo lsmod | grep zfs
+sudo systemctl list-unit-files | grep zfs
+
+echo ''
+echo "=============================================="
+echo "Done: Finish OpenZFS configuration.           "
+echo "=============================================="
+echo ''
+
+sleep 5
+
+clear
+
+echo ''
+echo "=============================================="
+echo "Create zpool...                               "
+echo "=============================================="
+echo ''
+
+sudo zpool create $1 mirror /dev/sdb /dev/sdc
+sudo zpool list
+
+echo ''
+echo "=============================================="
+echo "Done: Create zpool.                           "
+echo "=============================================="
+echo ''
+
+
--- zfsutils/redhat/zpool_redhat_8.sh
+++ zfsutils/redhat/zpool_redhat_8.sh
@@ -37,18 +37,35 @@ clear
 
 echo ''
 echo "=============================================="
+echo "Configure Required Repos ...                  "
+echo "=============================================="
+echo ''
+
+sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
+sudo yum config-manager --enable epel
+source /etc/os-release
+sudo dnf install https://zfsonlinux.org/epel/zfs-release.el${VERSION_ID/./_}.noarch.rpm
+sudo rpm --import /etc/pki/rpm-gpg/RPM-GPG-KEY-zfsonlinux
+
+echo ''
+echo "=============================================="
+echo "Done: Configure Required Repos.               "
+echo "=============================================="
+echo ''
+
+sleep 5
+
+clear
+
+echo ''
+echo "=============================================="
 echo "Install packages ...                          "
 echo "=============================================="
 echo ''
 
 sudo yum -y install yum-utils
-sudo dnf -y install https://dl.fedoraproject.org/pub/epel/epel-release-latest-8.noarch.rpm
-sudo yum config-manager --enable epel
-sudo dnf install -y  http://download.zfsonlinux.org/epel/zfs-release.el8_4.noarch.rpm
 sudo dnf -y install kernel-devel gpg
 sudo dnf -y install zfs
-sudo modprobe zfs
-sudo systemctl list-unit-files | grep zfs
 
 echo ''
 echo "=============================================="
@@ -58,6 +75,28 @@ echo ''
 
 sleep 5
 
+clear
+
+echo ''
+echo "=============================================="
+echo "Verify ZFS Install...                         "
+echo "=============================================="
+echo ''
+
+sudo modprobe zfs
+sudo systemctl list-unit-files | grep zfs
+
+
+echo ''
+echo "=============================================="
+echo "Done: Verify ZFS Install.                     "
+echo "=============================================="
+echo ''
+
+sleep 5
+
+clear
+
 echo ''
 echo "=============================================="
 echo "Create ZFS Storage...                         "
@@ -66,9 +105,29 @@ echo ''
 
 sudo zpool create $1 mirror /dev/sdb /dev/sdc
 
+echo ''
+echo "=============================================="
+echo "Create ZFS Storage...                         "
+echo "=============================================="
+echo ''
+
+sleep 5
+
+clear
+
+echo ''
+echo "=============================================="
+echo "Verify ZFS Pool...                            "
+echo "=============================================="
+echo ''
+
 sudo zpool list
 sudo zpool status
 
+echo ''
+echo "=============================================="
+echo "Done: Verify ZFS Pool.                        "
+echo "=============================================="
 echo ''
 echo "=============================================="
 echo "Done: Create ZFS Storage.                     "
