353ceb97fc7311398a341318b2a318bbfcd3f7e2
gstanden <gilstanden@hotmail.com>
2022-04-24 18:51:31 +0000

    v7.0.0-alpha ELENA

--- uekulele/uekulele-services-1.sh
+++ uekulele/uekulele-services-1.sh
@@ -4548,12 +4548,12 @@ then
 			echo "=============================================="
 			echo ''
 
-			sudo sed -i '2,4{s/^/#/}' 					   /etc/network/openvswitch/stop_$IscsiVendor.sh
-			sudo sh -c "echo '/var/lib/snapd/snap/bin/lxc stop -f --all'	>> /etc/network/openvswitch/stop_$IscsiVendor.sh"
+			sudo sed -i '2,5{s/^/#/}' 					   /etc/network/openvswitch/stop_$IscsiVendor.sh
+		#	sudo sh -c "echo '/var/lib/snapd/snap/bin/lxc stop -f --all'	>> /etc/network/openvswitch/stop_$IscsiVendor.sh"
 			sudo sh -c "echo 'sudo lxc-stop -n $NameServerBase'		>> /etc/network/openvswitch/stop_$IscsiVendor.sh"
 			sudo sh -c "echo 'sudo umount /$StoragePoolName'		>> /etc/network/openvswitch/stop_$IscsiVendor.sh"
 			sudo sh -c "echo 'sudo snap stop lxd'				>> /etc/network/openvswitch/stop_$IscsiVendor.sh"
-			sudo sh -c "echo 'sudo umount /var/lib/lxc'			>> /etc/network/openvswitch/stop_$IscsiVendor.sh"
+		#	sudo sh -c "echo 'sudo umount /var/lib/lxc'			>> /etc/network/openvswitch/stop_$IscsiVendor.sh"
 			sudo sh -c "echo 'sudo service multipathd stop'			>> /etc/network/openvswitch/stop_$IscsiVendor.sh"
 			sudo sh -c "echo 'sudo iscsiadm -m node --logout'		>> /etc/network/openvswitch/stop_$IscsiVendor.sh"
 			sudo sh -c "echo 'sudo service scst stop'			>> /etc/network/openvswitch/stop_$IscsiVendor.sh"
